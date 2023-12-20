3bbca1245654c5c70ecb2935e3a91a40b5a266c1
gtennent-tb <gtennent@travelbank.com>
2023-12-20 12:16:03 -0500

    FIle is pulling correctly, just need business logic now

--- src/utils/custom-expense-export/generateBoilerplateEjs.ts
+++ src/utils/custom-expense-export/generateBoilerplateEjs.ts
@@ -2,11 +2,31 @@
    This is a workflow automation designed to speed up the development of custom files by avoiding writing customer-agnostic code for every new custom export. */
 
 import { toCamelCase } from '../string-utils/toCamelCase';
+import { parse } from 'csv-parse';
+import fs, { ReadStream } from 'fs';
 
-export const generateBoilerplateEjs = (columnHeaders: string[]) => {
-  return `<% rows = expenses.map((expense) => {return \`${columnHeaders
+const parseCsv = (fileName: string): string => {
+  const stream: ReadStream = fs.createReadStream(fileName);
+  let data = '';
+
+  stream.on('data', (chunk) => {
+    data += chunk.toString();
+  });
+  stream.on('end', () => {
+    console.log(data);
+  });
+
+  const parsedRows = data.split('\n');
+
+  return `file contents: ${JSON.stringify(parsedRows)}`;
+};
+
+export const generateBoilerplateEjs = (fileName: string): string => {
+  return parseCsv(fileName);
+
+  /* return `<% rows = expenses.map((expense) => {return \`${columnHeaders
     .map((header) => `const ${toCamelCase(header)} = '';`)
     .join(
       ',',
-    )}\`;});%><% rows.unshift(${columnHeaders}) -%><%- rows.join('\\n'); -%>`;
+    )}\`;});%><% rows.unshift(${columnHeaders}) -%><%- rows.join('\\n'); -%>`;*/
 };
